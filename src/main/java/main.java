7beac7a7789758eccbbd72555e5c967d0f722b24
Alen <alenhasana@yahoo.dk>
2020-01-07 11:40:18 +0100

    ddd

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,29 +6,9 @@ public class main {
     public static void main(String[] args) {
 
         System.out.println("Welcome");
-        calcualte();
 
     }
 
-
-    public static int calcualte() {
-        int a, b, total;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Write 2 numbers");
-        a = scanner.nextInt();
-        b = scanner.nextInt();
-
-
-        return plus(a,b);
-    }
-
-    public static int plus(int a, int b) {
-        int total;
-
-        total = a + b;
-
-        return total;
-    }
 
 
 }
